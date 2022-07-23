871e57deadee06c28b9c30f94e33ac3de6c54c7c
AlvinAntony <aaalvinantony@gmail.com>
2022-07-23 15:32:48 +0530

    changes

--- pages/campaign/[id]/requests/index.js
+++ pages/campaign/[id]/requests/index.js
@@ -106,20 +106,14 @@ const RequestRow = ({
       setLoadingFinalize(false);
     }
   };
-  const color1 = useColorModeValue("teal.100", "teal.700");
-  const color2 = useColorModeValue("gray.100", "gray.700");
-  const color3 = useColorModeValue("white", "gray.700");
-  const color4 = useColorModeValue("gray.800", "white");
-  const color5 = useColorModeValue("green.600", "green.300");
-  const color6 = useColorModeValue("teal.800", "white");
-  const color7 = useColorModeValue("gray.600", "gray.300");
-
-  const bp1 = useBreakpointValue({ base: "left" });
-  const bp2 = useBreakpointValue({ base: "center" });
 
   return (
     <Tr
-      bg={readyToFinalize && !request.complete ? color1 : color2}
+      bg={
+        readyToFinalize && !request.complete
+          ? useColorModeValue("teal.100", "teal.700")
+          : useColorModeValue("gray.100", "gray.700")
+      }
       opacity={request.complete ? "0.4" : "1"}
     >
       <Td>{id} </Td>
@@ -145,9 +139,9 @@ const RequestRow = ({
         <HStack spacing={2}>
           <Tooltip
             label={errorMessageApprove}
-            bg={color3}
+            bg={useColorModeValue("white", "gray.700")}
             placement={"top"}
-            color={color4}
+            color={useColorModeValue("gray.800", "white")}
             fontSize={"1em"}
           >
             <WarningIcon
@@ -158,12 +152,14 @@ const RequestRow = ({
           {request.complete ? (
             <Tooltip
               label="This Request has been finalized & withdrawn to the recipient,it may then have less no of approvers"
-              bg={color3}
+              bg={useColorModeValue("white", "gray.700")}
               placement={"top"}
-              color={color4}
+              color={useColorModeValue("gray.800", "white")}
               fontSize={"1em"}
             >
-              <CheckCircleIcon color={color5} />
+              <CheckCircleIcon
+                color={useColorModeValue("green.600", "green.300")}
+              />
             </Tooltip>
           ) : (
             <Button
@@ -185,9 +181,9 @@ const RequestRow = ({
       <Td>
         <Tooltip
           label={errorMessageFinalize}
-          bg={color3}
+          bg={useColorModeValue("white", "gray.700")}
           placement={"top"}
-          color={color4}
+          color={useColorModeValue("gray.800", "white")}
           fontSize={"1em"}
         >
           <WarningIcon
@@ -199,12 +195,14 @@ const RequestRow = ({
         {request.complete ? (
           <Tooltip
             label="This Request has been finalized & withdrawn to the recipient,it may then have less no of approvers"
-            bg={color3}
+            bg={useColorModeValue("white", "gray.700")}
             placement={"top"}
-            color={color4}
+            color={useColorModeValue("gray.800", "white")}
             fontSize={"1em"}
           >
-            <CheckCircleIcon color={color5} />
+            <CheckCircleIcon
+              color={useColorModeValue("green.600", "green.300")}
+            />
           </Tooltip>
         ) : (
           <HStack spacing={2}>
@@ -224,14 +222,14 @@ const RequestRow = ({
 
             <Tooltip
               label="This Request is ready to be Finalized because it has been approved by 50% Approvers"
-              bg={color3}
+              bg={useColorModeValue("white", "gray.700")}
               placement={"top"}
-              color={color4}
+              color={useColorModeValue("gray.800", "white")}
               fontSize={"1.2em"}
             >
               <InfoIcon
                 as="span"
-                color={color6}
+                color={useColorModeValue("teal.800", "white")}
                 display={
                   readyToFinalize && !request.complete ? "inline-block" : "none"
                 }
@@ -340,18 +338,13 @@ export default function Requests({
           ) : null}
         </Container>
         {requestsList.length > 0 ? (
-          <Container
-            px={{ base: "4", md: "12" }}
-            maxW={"7xl"}
-            align={"left"}
-            height={"100vh"}
-          >
+          <Container px={{ base: "4", md: "12" }} maxW={"7xl"} align={"left"}>
             <Flex flexDirection={{ base: "column", lg: "row" }} py={4}>
               <Box py="2" pr="2">
                 <Heading
-                  textAlign={bp1}
+                  textAlign={useBreakpointValue({ base: "left" })}
                   // fontFamily={"heading"}
-                  color={color4}
+                  color={useColorModeValue("gray.800", "white")}
                   as="h3"
                   isTruncated
                   maxW={"3xl"}
@@ -436,9 +429,8 @@ export default function Requests({
               display={
                 requestsList.length === 0 && !isLoading ? "block" : "none"
               }
-              height={"70vh"}
             >
-              <SimpleGrid row spacing={3} align="center">
+              <SimpleGrid row spacing={2} align="center">
                 <Stack align="center">
                   <NextImage
                     src="/static/no-requests.png"
@@ -447,10 +439,19 @@ export default function Requests({
                     height="150"
                   />
                 </Stack>
-                <Heading textAlign={"center"} color={color4} as="h4" size="md">
+                <Heading
+                  textAlign={"center"}
+                  color={useColorModeValue("gray.800", "white")}
+                  as="h4"
+                  size="md"
+                >
                   No Requests yet for {name} Campaign
                 </Heading>
-                <Text textAlign={bp2} color={color7} fontSize="sm">
+                <Text
+                  textAlign={useBreakpointValue({ base: "center" })}
+                  color={useColorModeValue("gray.600", "gray.300")}
+                  fontSize="sm"
+                >
                   Create a Withdrawal Request to Withdraw funds from the
                   Campaign😄
                 </Text>
